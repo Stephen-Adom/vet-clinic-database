00244665fb2088e4a1249130266a362c376ae2a8
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-01 12:29:57 +0000

    add sql commands to data.sql, queries.sql and schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animal (id BIGSERIAL, name VARCHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL);
