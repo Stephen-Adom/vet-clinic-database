f776e722fa3f0a9a6d6c6c519797b5eda36c4bbd
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-01 14:32:53 +0000

    add primary key to animal id schema and update the queries for selecting animal

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 SELECT * FROM animal WHERE name LIKE '%mon';
 
-SELECT name from animal WHERE date_of_birth  >= '2016-01-01' AND date_of_birth <= '2019-01-01';
+SELECT name from animal WHERE date_of_birth  >= '2016-01-01' AND date_of_birth <= '2019-12-31';
 
 SELECT name FROM animal WHERE neutered = true AND escape_attempts < 3;
 
--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animal (id BIGSERIAL, name VARCHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL);
+CREATE TABLE animal (id BIGSERIAL PRIMARY KEY, name VARCHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL);
