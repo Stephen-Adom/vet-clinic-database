1fbd5e1694a9b9472dddbd08c8b54bc6dc9bb38a
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-02 10:07:21 +0000

    add alter table command to add species column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animal (id BIGSERIAL PRIMARY KEY, name VARCHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOL, weight_kg DECIMAL);
+
+ALTER TABLE animal ADD COLUMN species VARCHAR(100);
