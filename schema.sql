4ba688756390c4539413e16cc70eae342e69f94c
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-09 10:57:49 +0000

    add schema queries

--- schema.sql
+++ schema.sql
@@ -27,3 +27,71 @@ CREATE TABLE visit(animal_id INT, vet_id INT, PRIMARY KEY(animal_id, vet_id));
 ALTER TABLE visit ADD COLUMN date_of_visit DATE;
 
 ALTER TABLE visit DROP CONSTRAINT visit_pkey;
+
+
+CREATE TABLE "Species" (
+  "id" BIGSERIAL,
+  "name" VARCHAR(150),
+  "Field" Type,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Animal" (
+  "id" BIGSERIAL,
+  "name" VARCHAR(50),
+  "date_of_birth" DATE,
+  "escape_attempts" INT,
+  "neutered" BOOL,
+  "weight_kg" DECIMAL,
+  "species_id" INT,
+  "owner_id" INT,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_Animal.species_id"
+    FOREIGN KEY ("species_id")
+      REFERENCES "Species"("id")
+);
+
+CREATE TABLE "Owner" (
+  "id" BIGSERIAL,
+  "full_name" VARCHAR(50),
+  "age" INT,
+  "email" VARCHAR(150),
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_Owner.id"
+    FOREIGN KEY ("id")
+      REFERENCES "Animal"("owner_id")
+);
+
+CREATE TABLE "Vet" (
+  "id" BIGSERIAL,
+  "name" VARCHAR(150),
+  "date_of_graduation" DATE,
+  "age" INT,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Specialization" (
+  "species_id" INT,
+  "vet_id" INT,
+  PRIMARY KEY ("species_id", "vet_id"),
+  CONSTRAINT "FK_Specialization.vet_id"
+    FOREIGN KEY ("vet_id")
+      REFERENCES "Vet"("id"),
+  CONSTRAINT "FK_Specialization.species_id"
+    FOREIGN KEY ("species_id")
+      REFERENCES "Species"("name")
+);
+
+CREATE TABLE "Visit" (
+  "animal_id" INT,
+  "vet_id" INT,
+  "date_of_visit" DATE,
+  PRIMARY KEY ("animal_id", "vet_id"),
+  CONSTRAINT "FK_Visit.vet_id"
+    FOREIGN KEY ("vet_id")
+      REFERENCES "Vet"("id"),
+  CONSTRAINT "FK_Visit.animal_id"
+    FOREIGN KEY ("animal_id")
+      REFERENCES "Animal"("id")
+);
+
